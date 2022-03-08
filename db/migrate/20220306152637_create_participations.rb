395f22252aeb24c8a313c1c2db32e2f6045ad03f
alfieyfc <alfieyfc@gmail.com>
2022-03-08 10:28:32 +0000

    fix syntax

--- db/migrate/20220306152637_create_participations.rb
+++ db/migrate/20220306152637_create_participations.rb
@@ -1,6 +1,6 @@
 class CreateParticipations < ActiveRecord::Migration[7.0]
   def change
-    create_table :participations,  do |t|
+    create_table :participations do |t|
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
 
